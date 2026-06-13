8439f2d2b2c057853ac5d46f8718ea32322e7519
lazyboyharsh <hharshjain448@gmail.com>
2026-06-13 23:01:17 +0530

    Initial commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -348,18 +348,17 @@ export default function Footer() {
             </div>
 
             {/* CREDIT */}
-            {/* <div className="text-sm text-black/50">
-              Design & Developed by{" "}
-
-              <a
-                href="https://www.lazyworkz.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-semibold text-black transition hover:text-blue-600"
-              >
-                LazyWorkz
-              </a>
-            </div> */}
+            <div className="text-sm text-[#F4F0E8]/50">
+  Design & Developed by{" "}
+  <a
+    href="https://www.lazyworkz.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="font-semibold text-[#F4F0E8] transition hover:text-[#F4F0E8]/80"
+  >
+    LazyWorkz
+  </a>
+</div>
 
           </div>
         </div>
